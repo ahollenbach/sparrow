fd09334f6d04bbe2309ddb6dafaffca50d93bc65
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2016-05-12 16:24:25 -0400

    Fix worker

--- src/Worker.py
+++ src/Worker.py
@@ -107,8 +107,8 @@ if __name__ == "__main__":
     hostname = socket.gethostname()
     #worker_number = find_worker_number()
     worker_number = raw_input("Please enter worker number")
-    if worker_number != 99:
-        name_in_nameserver = "sparrow.worker" + str(worker_number + 1)
+    if worker_number != "99":
+        name_in_nameserver = "sparrow.worker" + str(int(worker_number) + 1)
         Pyro4.Daemon.serveSimple(
             {
                 Worker(worker_number): name_in_nameserver
